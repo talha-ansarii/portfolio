36a459fe41a7c29a23caf0d7f12e5331c3827549
Talha Ansari <talhaansari1606@gmail.com>
2024-11-01 01:07:41 +0530

    mobile4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
       icon: (
         <IconRoute className="h-full w-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: "/journey",
+      href: "/experience",
     },
     {
       title: "Skills",
